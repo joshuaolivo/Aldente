22ea427f0c4938954cd8df0076390f5668735bdf
JoshuaSoleraHPOL <joshua.olivo@solera.com>
2023-01-11 15:32:35 -0600

    Sistema de usuarios terminado

--- Aldente/Aldente/Models/LoginModel.cs
+++ Aldente/Aldente/Models/LoginModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,15 @@ namespace Aldente.Models
 {
     public class LoginModel
     {
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [EmailAddress(ErrorMessage = "El campo debe ser un correo electrónico válido")]
+        public string Email { get; set; } = null!;
+
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+
+        [Display(Name = "Recuérdame")]
+        public bool Recuerdame { get; set; }
     }
 }
--- Aldente/Aldente/Models/SingInModel.cs
+++ Aldente/Aldente/Models/SingInModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,11 @@ namespace Aldente.Models
 {
     public class SingInModel
     {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
     }
 }
