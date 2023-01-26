176a09559f09b0dbd94d3204d864569591c117fe
JoshuaSoleraHPOL <joshua.olivo@solera.com>
2023-01-26 07:34:37 -0600

    DateRaul

--- Aldente/Aldente/DTOs/RestauranteDTO.cs
+++ Aldente/Aldente/DTOs/RestauranteDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,11 @@ namespace Aldente.DTOs
 {
     public class RestauranteDTO : PlatillaRestauranteDTO
     {
+        [Required]
+        [EmailAddress]
         public string Correo { get; set; }
+        [RegularExpression("(^[0-9]+$)", ErrorMessage = "Solo se permiten números")]
+        [StringLength(13, ErrorMessage = "El número es demasiado largo")]
         public string Telefono { get; set; }
         public string Direccion { get; set; }
     }
